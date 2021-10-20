402bb7f4c2cf5b782fe676edfe0c4e75567ed937
Mehmet Cenk Yeniköylü <mcy@dotnetmcy.net>
2021-10-20 14:27:29 +0300

    doc, img, folders, coverPage xaml, menu

--- DXUwpApplication1/CoverPage.xaml.cs
+++ DXUwpApplication1/CoverPage.xaml.cs
@@ -10,6 +10,26 @@ namespace DXUwpApplication1
         public CoverPage()
         {
             this.InitializeComponent();
+
+            // Set supported inking device types.
+            myInkCanvas.InkPresenter.InputDeviceTypes =
+                Windows.UI.Core.CoreInputDeviceTypes.Mouse |
+                Windows.UI.Core.CoreInputDeviceTypes.Pen;
+        }
+
+        private void CoverPageFileNotesSave_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnCoverPageNew_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnCoverPagePDF_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+
         }
     }
 }
